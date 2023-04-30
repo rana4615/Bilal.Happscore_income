d68c3f9bd9ac9b45fd62cf939e3eef55b61e8c7b
rana4615 <132162476+rana4615@users.noreply.github.com>
2023-05-01 00:03:02 +0500

    Add files via upload

---
+++ Bilal.Happyscore.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[29]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+# In[30]:
+
+
+happy=pd.read_csv("C:\\Users\\My Pc\\Downloads\\happyscore_income.csv")
+
+
+# In[31]:
+
+
+happy
+
+
+
+
+
+# In[32]:
+
+
+happy.plot()
+plt.grid()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
